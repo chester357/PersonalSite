28426f1dccca481e19e7cbeff8596c798ac1df4b
Chester Schofield <Chester@Chesters-MacBook-Pro.local>
2015-08-21 17:42:58 -0500

    Pushing to heroku

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+app.use(express.static(__dirname + '/views'));
+
+app.get('/', function(req, res){
+	res.sendFile('index.html');
+});
+
+app.listen(3000, function(){
+	console.log("Slangin on localhost:3000");
+});
