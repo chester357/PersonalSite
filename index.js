3d664f8d1ac27b42eb52273c91960aae3274b4e2
Chester Schofield <Chester@Chesters-MacBook-Pro.local>
2015-08-21 20:03:29 -0500

    index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res){
 	res.sendFile('index.html');
 });
 
-app.listen(3000, function(){
-	console.log("Slangin on localhost:3000");
+app.listen(app.get('port') function(){
+	console.log("Slangin on", app.get('port'));
 });
