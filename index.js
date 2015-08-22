e9a1804f79d0ec343f5b9c5788a969bf433bb1bf
Chester Schofield <Chester@Chesters-MacBook-Pro.local>
2015-08-21 20:07:53 -0500

    port numbber

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express');
-var path = require('path');
+// var path = require('path');
 
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res){
 	res.sendFile('index.html');
 });
 
-app.listen(app.get('port') function(){
+app.listen(app.get('port'), function(){
 	console.log("Slangin on", app.get('port'));
 });
